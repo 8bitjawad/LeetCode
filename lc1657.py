cd279241d81a90e34a1e0892689dd3909cd217e4
Jawad <mjawadhussain4@gmail.com>
2025-10-19 22:49:41 +0530

    new

---
+++ lc1657.py
@@ -0,0 +1,19 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+
+        if len(word1) != len(word2):
+            return False
+
+        set1 = set(word1)
+        set2 = set(word2)
+
+        if set1 != set2:
+            return False
+        
+        freq1 = Counter(word1)
+        freq2 = Counter(word2)
+
+        if sorted(freq1.values()) != sorted(freq2.values()):
+            return False
+
+        return True        
